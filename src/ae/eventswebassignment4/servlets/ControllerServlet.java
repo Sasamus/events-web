a28392a6c0d367ec14af7c66057b2136c36e3af2
Sasamus <labus93@gmail.com>
2014-12-17 10:51:11 +0100

    All data reading from file works, exceot for Organizer

--- src/ae/eventswebassignment4/servlets/ControllerServlet.java
+++ src/ae/eventswebassignment4/servlets/ControllerServlet.java
@@ -3,6 +3,7 @@ package ae.eventswebassignment4.servlets;
 import java.io.IOException;
 
 import javax.ejb.EJB;
+import javax.ejb.Singleton;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,22 +11,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import ae.eventsbusinessassignment4.databasemanaging.DatabaseManager;
+import ae.eventsbusinessassignment4.databasemanaging.DatabaseManagerBean;
 
 /**
  * Servlet implementation class ControllerServlet
  * 
  * @author Albin Engström
  */
+@Singleton
 @WebServlet("/ControllerServlet")
 public class ControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	//TODO: Page counters shared?
+	//TODO: Synchronization
 
 	/**
 	 * A DatabaseManagerBean object
 	 */
-	@EJB(beanInterface = DatabaseManager.class)//(beanName = "DatabaseManagerBean")
-	private static DatabaseManager databaseManager;
+	@EJB(beanName = "DatabaseManagerBean")
+	private DatabaseManagerBean databaseManagerBean;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -41,11 +46,11 @@ public class ControllerServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
-		// Call it readData()
-		//databaseManager.readData();
+		// Call readData()
+		databaseManagerBean.readData();
 
 		// Create page
-		//createEventsOverview(request, response, "all");
+		createEventsOverview(request, response, "all");
 	}
 
 	/**
