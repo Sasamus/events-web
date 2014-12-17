62b619e4eda0b3406e85ad791610f487f3fd7b03
Sasamus <labus93@gmail.com>
2014-12-17 19:05:32 +0100

    Migrated doPost from assignment3

--- src/ae/eventswebassignment4/servlets/ControllerServlet.java
+++ src/ae/eventswebassignment4/servlets/ControllerServlet.java
@@ -1,6 +1,8 @@
 package ae.eventswebassignment4.servlets;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,6 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import ae.eventsbusinessassignment4.databasemanaging.DatabaseManagerBean;
+import ae.eventsbusinessassignment4.entities.Comment;
 import ae.eventsbusinessassignment4.entities.Event;
 import ae.eventsbusinessassignment4.entities.User;
 
@@ -30,6 +33,7 @@ public class ControllerServlet extends HttpServlet {
 
 	// TODO: Page counters shared?
 	// TODO: Synchronization
+	// TODO: Entities bean name
 
 	/**
 	 * A DatabaseManagerBean object
@@ -56,6 +60,95 @@ public class ControllerServlet extends HttpServlet {
 
 		// Create page
 		createEventsOverview(request, response, "all");
+	}
+	
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		//Check what page to create
+		if(request.getParameter("AddEvent").equals("true")){
+			
+			//A List of User's
+			List<User> users = databaseManagerBean.getAllUsers();
+			
+			//Add users to request
+			request.setAttribute("allUsers", users);
+						
+			//Create AddVent
+			createAddEvent(request, response);
+		}
+		else if(request.getParameter("NewEvent").equals("true")){
+			
+			//An Event object
+			Event event = new Event();
+			
+			//Set it's values
+			event.setEventTitle(request.getParameter("title"));
+			event.setEventCity(request.getParameter("city"));
+			event.setEventDescription(request.getParameter("description"));
+			
+			//Get times
+			String startTime = request.getParameter("starttime");
+			String endTime = request.getParameter("endtime");
+			
+			//Create Timestamps with the time Strings
+			Timestamp timestampStart = Timestamp.valueOf(startTime);
+			Timestamp timestampEnd = Timestamp.valueOf(endTime);
+			
+			//Create a Timestamp with the current time
+			Timestamp currentTimestamp = new Timestamp(new Date().getTime());
+			
+			//Set event's time related fields
+			event.setEventStart(timestampStart);
+			event.setEventEnd(timestampEnd);
+			event.setLastUpdate(currentTimestamp);
+			
+			//Get Organizer
+			String userId = request.getParameter("organizerId");
+			
+			//The User
+			User user = databaseManagerBean.getUser(Integer.parseInt(userId));
+			
+			//Add Event
+			databaseManagerBean.addEvent(event, user);
+			
+			//Create EventsOverview
+			createEventsOverview(request, response, "all");
+			
+			
+		}
+		else if(request.getParameter("GoToUser").equals("true")){
+			
+			//Get the User Id
+			String id = request.getParameter("UserId");
+			
+			//Get the User with id
+			User user = databaseManagerBean.getUser(Integer.parseInt(id));
+			
+			//Get the Comments user have made
+			List<Comment> comments = databaseManagerBean.getUserComments(user);
+			
+			//Add size of comments to request
+			request.setAttribute("nrComments", comments.size());
+			
+			//Create UserProfile
+			createUserProfile(request, response, user);
+		}
+		else{
+			//Get filter city
+			String city = request.getParameter("filter");
+			
+			city = new String(city.getBytes("8859_1"),"UTF8");
+			
+			//Create EventsOverview
+			createEventsOverview(request, response, city);
+		}
+		
+		
 	}
 
 	/**
