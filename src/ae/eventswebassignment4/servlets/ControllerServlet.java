52b3a206861899f705eb52080ba1e9733ec59acf
Sasamus <labus93@gmail.com>
2014-12-17 16:27:22 +0100

    Migrated all of createEventsOverview

--- src/ae/eventswebassignment4/servlets/ControllerServlet.java
+++ src/ae/eventswebassignment4/servlets/ControllerServlet.java
@@ -1,6 +1,9 @@
 package ae.eventswebassignment4.servlets;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
@@ -12,6 +15,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import ae.eventsbusinessassignment4.databasemanaging.DatabaseManagerBean;
+import ae.eventsbusinessassignment4.entities.Event;
+import ae.eventsbusinessassignment4.entities.User;
 
 /**
  * Servlet implementation class ControllerServlet
@@ -67,12 +72,47 @@ public class ControllerServlet extends HttpServlet {
 			HttpServletResponse response, String city) throws ServletException,
 			IOException {
 
-		// Get a RequestDispatcher for EventsOverview
-		RequestDispatcher requestdispatcher = request
-				.getRequestDispatcher("/EventsOverview.jsp");
-
-		// Forward request
-		requestdispatcher.forward(request, response);
+		List<Event> events = null;
+		if(city.equals("all") || city.equals("")){
+			
+			//Get all Events and put them in events
+			events = databaseManagerBean.getAllEvents();
+		}
+		else{
+			
+			//Get Events and put them in events
+			events = databaseManagerBean.getCityEvents(city);
+		}
+		
+		
+		//Add events to request
+		request.setAttribute("eventsList", events);
+		
+		//A Map using Event's as keys and stores List's of User's
+		Map<Event, List<User>> organizerUserMap = new HashMap<Event, List<User>>();
+		
+		//Loop through events
+		for(Event event : events){
+			
+			//Get the User's that are Organizer's for event
+			List<User> users = databaseManagerBean.getEventOrganizers(event);
+			
+			//Put the List in organizerUserMap
+			organizerUserMap.put(event, users);	
+		}
+		
+		//Add organizerUserMap to request
+		request.setAttribute("organizerUserMap", organizerUserMap);
+		
+		//Increment eventsOverviewCallCounter
+		//TODO: Uncomment
+		//incrementEventsOverviewCallCounter(request);
+		
+		//Get a RequestDispatcher for EventsOverview
+		RequestDispatcher requestdispatcher = request.getRequestDispatcher("/EventsOverview.jsp");
+		
+		//Forward request
+        requestdispatcher.forward(request, response);
 
 	}
 }
